0a45dc2316680b11896ebadc4e7017d15b5ac7c8
mrrbrilliant <mrrbrilliantwork@gmail.com>
2020-11-30 09:08:49 +0700

    update config path

--- pi-library/src/utils/config.rs
+++ pi-library/src/utils/config.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 pub fn get() -> Config {
-    let file = File::open("root/etc/pi-conf.json").unwrap();
+    let file = File::open("development/manager/root/etc/pi-conf.json").unwrap();
     let mut buf_reader = BufReader::new(file);
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents).unwrap();
